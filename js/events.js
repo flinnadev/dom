7704cb61cf58ff90754fd19629ecf755417fbcd9
Sof <flinna@proton.me>
2024-06-09 18:23:42 +0300

    Add events.js

---
+++ js/events.js
@@ -0,0 +1,23 @@
+const signUpBtn = document.querySelector("#signup");
+console.dir(signUpBtn);
+
+function eventHandler(event) {
+  console.log(event);
+  alert("Sign in");
+}
+
+signUpBtn.onclick = eventHandler;
+//signUpBtn.onclick = null;
+
+//2
+const LoginBtn = document.querySelector("#login");
+
+const loginEventHandler = (e) => alert("You are successfully login");
+
+LoginBtn.addEventListener("click", (e) => alert("You are successfully login"));
+LoginBtn.removeEventListener("click", (e) =>
+  alert("You are successfully login")
+);
+
+const h1 = document.querySelector("h1");
+h1.onmouseenter = (e) => alert("welcome");
