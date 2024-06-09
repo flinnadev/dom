fb285bd900b80faaff3fc3237ca48a3fae0fe694
Sof <flinna@proton.me>
2024-06-09 17:27:22 +0300

    Add js

---
+++ js/accetss.js
@@ -0,0 +1,21 @@
+console.log("document :>>", document);
+
+console.dir(document);
+
+console.log("document.body :>>", document.body);
+
+console.log(document.getElementsByTagName("h1"));
+
+console.log(document.getElementsByTagName("section"));
+
+console.log(document.getElementById("root"));
+
+const h1 = document.querySelector("h1");
+console.log(h1);
+
+const sectionEls = document.querySelectorAll(".news");
+
+console.log(sectionEls);
+s;
+console.log(sectionEls[0]);
+console.log(sectionEls[1]);
