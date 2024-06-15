4a780ec273159e6f74224259c0f86b09a6d47270
Sof <flinna@proton.me>
2024-06-15 16:04:08 +0300

    Add css

--- js/attributes.js
+++ js/attributes.js
@@ -1,7 +1,7 @@
-const h1 = document.querySelector("h1");
-console.dir(h1);
-
-h1.title = "It's heading";
-
-h1.style.backgroundColor = "green";
-h1.style.color = "yellow";
+//const h1 = document.querySelector("h1");
+//console.dir(h1);
+//
+//h1.title = "It's heading";
+//
+//h1.style.backgroundColor = "green";
+//h1.style.color = "yellow";
